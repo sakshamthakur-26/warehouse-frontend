819984791642810886225aa07166e905944c0cbb
Sumit Patel <2492268@cognizant.com>
2026-06-19 13:53:51 +0530

    initial commit

---
+++ src/app/vendors/vendor-form/vendor-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-form',
+  imports: [],
+  templateUrl: './vendor-form.html',
+  styleUrl: './vendor-form.css',
+})
+export class VendorForm {
+
+}
---
+++ src/app/vendors/vendor-list/vendor-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vendor-list',
+  imports: [],
+  templateUrl: './vendor-list.html',
+  styleUrl: './vendor-list.css',
+})
+export class VendorList {
+
+}
